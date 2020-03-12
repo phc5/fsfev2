2b87c6f35a878d3989cdbc55ca76298cc97fff52
Paul Chong <paulhyunchong@gmail.com>
2020-03-11 23:41:28 -0700

    add custom header

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('Hello World server');
+  res.send('Hello World server');
+});
+
+app.get('/418', (req, res) => {
+  res.set('X-custom-header', '418');
+  res.status(418);
+  res.send('This is a 418');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
